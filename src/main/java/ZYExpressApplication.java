10a826fcc5e3b70c4d02bdcbf057624cd67b0081
George Fally <george.fally@outlook.com>
2016-03-22 23:19:02 +0900

    minimal workable website

---
+++ src/main/java/ZYExpressApplication.java
@@ -0,0 +1,27 @@
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.forms.MultiPartBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class ZYExpressApplication extends Application<ZYExpressConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new ZYExpressApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "ZYExpressApplication";
+    }
+
+    @Override
+    public void initialize(Bootstrap<ZYExpressConfiguration> bootstrap) {
+        bootstrap.addBundle(new MultiPartBundle());
+        bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
+    }
+
+    @Override
+    public void run(ZYExpressConfiguration configuration, Environment environment) throws Exception {
+
+    }
+}
---
+++ src/main/java/ZYExpressConfiguration.java
@@ -0,0 +1,6 @@
+import io.dropwizard.Configuration;
+
+public class ZYExpressConfiguration extends Configuration {
+
+}
+
